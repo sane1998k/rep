80553f1808a9aab7af00039584e1560522269a70
sane1998k <gansterssss40@gmail.com>
2021-02-10 11:42:14 +0300

    graphtesting

--- php/dos1.php
+++ php/dos1.php
@@ -11,8 +11,7 @@ if (!$link) {
 if ($result = mysqli_query($link, "SELECT * FROM Report WHERE dateStart BETWEEN '".$_POST['dateFrom']."' AND '".$_POST['dateTo']."'")) {
    $sub_array = array();
    while( $row = mysqli_fetch_assoc( $result ) ) { 
-      array_push($sub_array, [$row['dateStart'], 
-      $row['dateEnd'], $row['task'], $row['dosatorSum1'], $row['dosatorSum2'], $row['dosatorSum3'],$row['totalAmount'], $row['resultSum']]);
+      array_push($sub_array, [$row['dateStart'], (float) $row['dosatorSum1']]);
    }
    $userData = $sub_array;
         $data['status'] = 'ok';
